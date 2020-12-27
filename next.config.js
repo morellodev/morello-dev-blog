bc0ef9ff8dbfe839d51687e98a092b5ff66f8c72
dennismorello <dennis.morello@contentwise.com>
2020-12-27 11:39:13 +0100

    fix(redirects): added posts redirects to new URLs

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ["www.datocms-assets.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/posts/:slug",
+        destination: "/:slug",
+        permanent: true,
+      },
+    ];
+  },
 };
