0be26d975ce3be0ce557e3af87bf12f055d9e6db
dennismorello <dennis.morello@contentwise.com>
2020-12-27 11:34:01 +0100

    feat(image): added next-image and link to personal website

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,14 +1,24 @@
+import Image from "next/image";
+
 export default function Avatar({ name, picture }) {
   return (
-    <div className="flex items-center space-x-4">
-      {picture && (
-        <img
-          src={picture.url}
-          className="w-12 h-12 rounded-full md:w-16 md:h-16"
-          alt={name}
-        />
-      )}
-      {name && <div className="font-bold md:text-xl">{name}</div>}
-    </div>
+    <a href="https://morello.dev" rel="noopener" target="_blank">
+      <div className="flex items-center space-x-4">
+        {picture && (
+          <div className="w-12 h-12 md:w-16 md:h-16">
+            <Image
+              src={picture.url}
+              alt={name}
+              className="rounded-full"
+              layout="intrinsic"
+              width={64}
+              height={64}
+              priority
+            />
+          </div>
+        )}
+        {name && <div className="font-bold md:text-xl">{name}</div>}
+      </div>
+    </a>
   );
 }
